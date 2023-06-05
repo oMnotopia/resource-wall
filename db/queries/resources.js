b1583d827d4dcd90799fdd8ee24c1f5a45912823
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-05 13:11:44 -0600

    changed getResources query to include Likes

--- db/queries/resources.js
+++ db/queries/resources.js
@@ -1,8 +1,8 @@
 const db = require('../connection');
 
-// Get all resources
+// Get all resources: return all resources with the Likes count, Order by resource_id
 const getResources = () => {
-  return db.query('SELECT * FROM resources;')
+  return db.query('SELECT r.id, r.url, r.rating, r.comments, r.category, COUNT(ur.user_id) AS Likes FROM resources r LEFT JOIN user_resources ur ON r.id = ur.resource_id GROUP BY r.id ORDER BY r.id;')
     .then(data => {
       return data.rows;
     });
@@ -10,7 +10,7 @@ const getResources = () => {
 
 // Get 1 resource by the resource_id
 const getResourceById = (id) => {
-  return db.query('SELECT * FROM resources WHERE id = $1', [id])
+  return db.query('SELECT * FROM resources WHERE id = $1;', [id])
     .then((resource) => {
       return resource.rows[0];
     });
@@ -22,7 +22,7 @@ const deleteResourceById = (id) => {};
 
 // Get resources CREATED by a user_id
 const getCreatedResources = (user_id) => {
-  return db.query('SELECT * FROM resources WHERE user_id = $1', [user_id])
+  return db.query('SELECT * FROM resources WHERE user_id = $1;', [user_id])
     .then((resources) => {
       return resources.rows;
     });
@@ -30,7 +30,7 @@ const getCreatedResources = (user_id) => {
 
 // Get resources LIKED by a user_id
 const getLikedResources = (user_id) => {
-  return db.query('SELECT * FROM resources WHERE id IN (SELECT resource_id FROM user_resources WHERE user_id = $1)', [user_id])
+  return db.query('SELECT * FROM resources WHERE id IN (SELECT resource_id FROM user_resources WHERE user_id = $1);', [user_id])
     .then((resources) => {
       return resources.rows;
     });
@@ -38,7 +38,7 @@ const getLikedResources = (user_id) => {
 
 // Get resources that has category simlar to the input (using LIKE)
 const getResourceByCategory = (category) => {
-  return db.query('SELECT * FROM resources WHERE category LIKE "%$1%"', [category])
+  return db.query('SELECT * FROM resources WHERE category LIKE "%$1%";', [category])
     .then((resources) => {
       return resources.rows;
     });
