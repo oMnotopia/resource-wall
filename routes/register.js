7b89490b724b959e7108f23a819f0cdd2efaa64b
Clayton <persinger.clayton@gmail.com>
2023-06-05 14:14:41 -0700

    Create POST request to add new user

--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const router  = express.Router();
+const { addUser } = require('../db/queries/users');
+const cookieSessions = require('cookie-session');
 
 router.get('/', (req, res) => {
   res.render('register');
+});
+
+router.post('/', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const templateVars = {
+    name,
+    email,
+    password,
+  };
+
+  //Checks if any of the fields are empty
+  //Checks if email already exists
+
+  //Add user to database
+  addUser(templateVars);
+
+  res.redirect('/resources');
 });
 
 module.exports = router;
