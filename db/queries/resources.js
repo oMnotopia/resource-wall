56243629585492b4ca6cba8caa16bb20c0981929
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-05 13:49:13 -0600

    updated queries

--- db/queries/resources.js
+++ db/queries/resources.js
@@ -1,16 +1,20 @@
 const db = require('../connection');
 
-// Get all resources: return all resources with the Likes count, Order by resource_id
+// Get all resources: return all resources with the Likes count of each resource, Order by resource_id
 const getResources = () => {
-  return db.query('SELECT r.id, r.url, r.rating, r.comments, r.category, COUNT(ur.user_id) AS Likes FROM resources r LEFT JOIN user_resources ur ON r.id = ur.resource_id GROUP BY r.id ORDER BY r.id;')
+  const query = 'SELECT r.id, r.url, r.rating, r.comments, r.category, COUNT(ur.user_id) AS Likes ' +
+    'FROM resources r LEFT JOIN user_resources ur ON r.id = ur.resource_id GROUP BY r.id ORDER BY r.id;'
+  return db.query(query)
     .then(data => {
       return data.rows;
     });
 };
 
-// Get 1 resource by the resource_id
+// Get 1 resource by the resource_id with the Likes count of the resource
 const getResourceById = (id) => {
-  return db.query('SELECT * FROM resources WHERE id = $1;', [id])
+  const query = 'SELECT r.id, r.url, r.rating, r.comments, r.category, COUNT(ur.user_id) AS Likes ' +
+    'FROM resources r LEFT JOIN user_resources ur ON r.id = ur.resource_id WHERE r.id = $1 GROUP BY r.id;';
+  return db.query(query, [id])
     .then((resource) => {
       return resource.rows[0];
     });
@@ -20,9 +24,12 @@ const updateResourceById = (id) => {};
 
 const deleteResourceById = (id) => {};
 
-// Get resources CREATED by a user_id
+// Get resources CREATED by a user_id, with the Likes count of each resource
 const getCreatedResources = (user_id) => {
-  return db.query('SELECT * FROM resources WHERE user_id = $1;', [user_id])
+  const query = 'SELECT r.id, r.url, r.rating, r.comments, r.category, COUNT(ur.user_id) AS Likes ' +
+    'FROM resources r LEFT JOIN user_resources ur ON r.id = ur.resource_id WHERE r.user_id = $1 ' +
+    'GROUP BY r.id ORDER BY r.id;';
+  return db.query(query, [user_id])
     .then((resources) => {
       return resources.rows;
     });
@@ -30,7 +37,10 @@ const getCreatedResources = (user_id) => {
 
 // Get resources LIKED by a user_id
 const getLikedResources = (user_id) => {
-  return db.query('SELECT * FROM resources WHERE id IN (SELECT resource_id FROM user_resources WHERE user_id = $1);', [user_id])
+  const query = 'SELECT r.*, COUNT(ur.resource_id) AS Likes '+
+    'FROM resources r LEFT JOIN user_resources ur ON r.id = ur.resource_id '+
+    'WHERE r.id IN (SELECT resource_id FROM user_resources WHERE user_id = $1) GROUP BY r.id;';
+  return db.query(query, [user_id])
     .then((resources) => {
       return resources.rows;
     });
@@ -38,7 +48,10 @@ const getLikedResources = (user_id) => {
 
 // Get resources that has category simlar to the input (using LIKE)
 const getResourceByCategory = (category) => {
-  return db.query('SELECT * FROM resources WHERE category LIKE "%$1%";', [category])
+  const query = "SELECT r.id, r.url, r.rating, r.comments, r.category, COUNT(ur.user_id) AS Likes " +
+  "FROM resources r LEFT JOIN user_resources ur ON r.id = ur.resource_id WHERE category LIKE '%$1%' " +
+  "GROUP BY r.id ORDER BY r.id;";
+  return db.query(query, [category])
     .then((resources) => {
       return resources.rows;
     });
