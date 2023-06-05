ff078550c0dec7bbec76b9b91a3c9193dbb27d62
Clayton <persinger.clayton@gmail.com>
2023-06-05 07:54:22 -0700

    Create psuedo code for futute code

--- routes/login.js
+++ routes/login.js
@@ -5,4 +5,19 @@ router.get('/', (req, res) => {
   res.render('login');
 });
 
+router.post('/', (req, res) => {
+  //Get login information
+  const userEmail = req.params.email;
+  const userPassword = req.params.password;
+
+  //Need error checking for missing fields
+
+  //Checks that email and password match existing user in database
+
+  //Calls function that returns sql data for user
+
+  //Redirects to user's resources page
+  res.redirect(`/resources/${1/*user id*/}`);
+});
+
 module.exports = router;
