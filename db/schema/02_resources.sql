94a8debb8c48fd56382a6248afb3abc58bc168fe
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-05 12:53:09 -0600

    removed Like field

--- db/schema/02_resources.sql
+++ db/schema/02_resources.sql
@@ -5,7 +5,6 @@ CREATE TABLE resources (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   url TEXT NOT NULL,
-  likes INTEGER NOT NULL,
   rating FLOAT(2) NOT NULL,
   comments TEXT NOT NULL,
   category VARCHAR(255) NOT NULL
