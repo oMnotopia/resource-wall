8712cf743852cf94293f66276c067b5e48e890a5
Clayton <persinger.clayton@gmail.com>
2023-06-06 14:44:21 -0700

    Update login route to check for existing user in database

--- routes/login.js
+++ routes/login.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const { getUsers } = require('../db/queries/users');
 
 router.get('/', (req, res) => {
   res.render('login');
@@ -7,17 +8,26 @@ router.get('/', (req, res) => {
 
 router.post('/', (req, res) => {
   //Get login information
-  const userEmail = req.params.email;
-  const userPassword = req.params.password;
+  const userEmail = req.body.email;
+  const userPassword = req.body.password;
 
-  //Need error checking for missing fields
-
-  //Checks that email and password match existing user in database
 
   //Calls function that returns sql data for user
-
-  //Redirects to user's resources page
-  res.redirect(`/resources/${1/*user id*/}`);
+  getUsers()
+    .then(response => {
+      console.log(response)
+      for (const user of response) {
+        //Checks that email and password match existing user in database
+        if (user.email === userEmail && user.password === userPassword) {
+          //Redirects to user's resources page
+          res.redirect(`/users/${user.id}/liked`);
+        }
+        //****NEED an error message for if email or password doesnt match */
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
