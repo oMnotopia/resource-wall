dfc219fa00dba56140eaa27cf1e25e4234d9a16c
Clayton <persinger.clayton@gmail.com>
2023-06-06 08:31:54 -0700

    Update route for single resource. Update resource page

--- routes/resources.js
+++ routes/resources.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const { getResources } = require('../db/queries/resources');
 const resourceQueries = require('../db/queries/resources');
+const { getResourceById } = require('../db/queries/resources');
 
 
 router.get('/', (req, res) => {
@@ -15,13 +17,23 @@ router.get('/', (req, res) => {
 
 });
 
-router.get('/:userid', (req, res) => {
-  const templateVars = {
-    username: "clayton",
-    email: "persinger.clayton@gmail.com",
-  };
+router.get('/:resourceid', (req, res) => {
+  const resourceId = req.params.resourceid;
+  getResourceById(resourceId)
+    .then(response => {
+      const templateVars = {
+        likes: response.like_count,
+        rating: response.average_rating,
+        title: response.title,
+        url: response.url,
+        imgUrl: response.img_url,
+        description: response.description,
+        category: response.category,
+      };
+      res.render('resource', templateVars);
+    });
 
-  res.render('user_resources', templateVars);
+
 });
 
 module.exports = router;
