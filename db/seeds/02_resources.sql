7e7cf96a1db75ee174db012374c0301b589a302b
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-05 22:58:40 -0600

    fixed typo

--- db/seeds/02_resources.sql
+++ db/seeds/02_resources.sql
@@ -1,18 +1,18 @@
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (1, 'Space title 1', 'https://www.youtube.com/watch?v=kQNqmUZrEOk','https://img.youtube.com/vi/kQNqmUZrEOk/0.jpg' 'Awesome', 'space');
+VALUES (1, 'Space title 1', 'https://www.youtube.com/watch?v=kQNqmUZrEOk', 'https://img.youtube.com/vi/kQNqmUZrEOk/0.jpg', 'Awesome', 'space');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (2, 'Code title 1', 'https://www.youtube.com/watch?v=B_n4YONte5A','https://img.youtube.com/vi/B_n4YONte5A/0.jpg' 'Really helpful','coding');
+VALUES (2, 'Code title 1', 'https://www.youtube.com/watch?v=B_n4YONte5A', 'https://img.youtube.com/vi/B_n4YONte5A/0.jpg', 'Really helpful','coding');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (2, 'Code title 2', 'https://pragmaticstudio.com/ruby', 'https://pragmaticstudio.com/assets/courses/ruby/ruby-large-38aa89bb013d067323c9b73043ffb56bff9ee5af88aa0e271333f4c963eb1907.png','Good Ruby tutorial', 'coding');
+VALUES (2, 'Code title 2', 'https://pragmaticstudio.com/ruby', 'https://pragmaticstudio.com/assets/courses/ruby/ruby-large-38aa89bb013d067323c9b73043ffb56bff9ee5af88aa0e271333f4c963eb1907.png', 'Good Ruby tutorial', 'coding');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (3, 'Code title 3', 'https://www.youtube.com/watch?v=W6NZfCO5SIk','https://img.youtube.com/vi/W6NZfCO5SIk/0.jpg' 'Javascript tutorial', 'coding');
+VALUES (3, 'Code title 3', 'https://www.youtube.com/watch?v=W6NZfCO5SIk', 'https://img.youtube.com/vi/W6NZfCO5SIk/0.jpg', 'Javascript tutorial', 'coding');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (3, 'Space title 2', 'https://www.youtube.com/watch?v=lqc0AQ3Rx7o','https://img.youtube.com/vi/lqc0AQ3Rx7o/0.jpg' 'Mysteries of the Universe', 'space');
+VALUES (3, 'Space title 2', 'https://www.youtube.com/watch?v=lqc0AQ3Rx7o', 'https://img.youtube.com/vi/lqc0AQ3Rx7o/0.jpg', 'Mysteries of the Universe', 'space');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (3, 'Space title 3', 'https://www.youtube.com/watch?v=hyNwBiCs7Ro','https://img.youtube.com/vi/hyNwBiCs7Ro/0.jpg' 'Unexplained Universe Mysteries', 'space');
+VALUES (3, 'Space title 3', 'https://www.youtube.com/watch?v=hyNwBiCs7Ro', 'https://img.youtube.com/vi/hyNwBiCs7Ro/0.jpg', 'Unexplained Universe Mysteries', 'space');
 
