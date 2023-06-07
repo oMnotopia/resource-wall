be8d1411aa85280bae4dbf1f79434a1d0ca283c6
Clayton <persinger.clayton@gmail.com>
2023-06-06 17:16:22 -0700

    Add cookie for get route, change format of data passed in post method to object

--- routes/register.js
+++ routes/register.js
@@ -1,24 +1,21 @@
 const express = require('express');
 const router  = express.Router();
 const { addUser } = require('../db/queries/users');
-const cookieSessions = require('cookie-session');
 
 router.get('/', (req, res) => {
-  res.render('register');
+  const templateVars = {
+    user: req.params.user_id
+  };
+  res.render('register', templateVars);
 });
 
 router.post('/', (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
-
   const templateVars = {
-    name,
-    email,
-    password,
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
   };
 
-  //Checks if any of the fields are empty
   //Checks if email already exists
 
   //Add user to database
