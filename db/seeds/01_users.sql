3fc6e782990a367171d7ca8fbe964e50e160fd6e
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-07 19:44:16 -0600

    typo

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,3 +2,5 @@
 INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@yahoo.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Kira', 'kslice@gmail.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Teddy', 'tedsteroni@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Terry Nguyen', 'terry@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Clayton P', 'clayton@gmail.com', 'password');
