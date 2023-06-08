8ca57ef251b075e505e644fab202982375f628b1
Terry Nguyen <terrynguyen100@gmail.com>
2023-06-07 21:15:48 -0600

    add more seeds

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,4 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@yahoo.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Kira', 'kslice@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Teddy', 'tedsteroni@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Alice', 'a@a.com', 'a');
 INSERT INTO users (name, email, password) VALUES ('Terry Nguyen', 'terry@gmail.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Clayton P', 'clayton@gmail.com', 'password');
--- db/seeds/02_resources.sql
+++ db/seeds/02_resources.sql
@@ -1,18 +1,71 @@
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (1, 'Space title 1', 'https://www.youtube.com/watch?v=kQNqmUZrEOk', 'https://img.youtube.com/vi/kQNqmUZrEOk/0.jpg', 'Awesome', 'space');
+VALUES (2, 'Building Web Apps with React', 'https://www.youtube.com/watch?v=PkZNo7MFNFg', 'https://img.youtube.com/vi/PkZNo7MFNFg/0.jpg', 'Learn the fundamentals of React and leverage its power to build dynamic web applications', 'coding');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (2, 'Code title 1', 'https://www.youtube.com/watch?v=B_n4YONte5A', 'https://img.youtube.com/vi/B_n4YONte5A/0.jpg', 'Really helpful','coding');
+VALUES (2, 'C++ Crash Course: Basics in One Hour', 'https://www.youtube.com/watch?v=NtfbWkxJTHw', 'https://img.youtube.com/vi/NtfbWkxJTHw/0.jpg', 'Get up to speed with C++ programming in just one hour with this crash course', 'coding');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (2, 'Code title 2', 'https://pragmaticstudio.com/ruby', 'https://pragmaticstudio.com/assets/courses/ruby/ruby-large-38aa89bb013d067323c9b73043ffb56bff9ee5af88aa0e271333f4c963eb1907.png', 'Good Ruby tutorial', 'coding');
+VALUES (3, 'Unraveling Enigmatic Universe Phenomena', 'https://www.youtube.com/watch?v=hyNwBiCs7Ro', 'https://img.youtube.com/vi/hyNwBiCs7Ro/0.jpg', 'Delve into the mysteries that perplex astronomers and scientists as they unravel enigmatic phenomena of the vast universe', 'space');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (3, 'Code title 3', 'https://www.youtube.com/watch?v=W6NZfCO5SIk', 'https://img.youtube.com/vi/W6NZfCO5SIk/0.jpg', 'Javascript tutorial', 'coding');
+VALUES (2, 'Python Automation: Simplifying Tasks', 'https://www.youtube.com/watch?v=ENrzD9HAZK4', 'https://img.youtube.com/vi/ENrzD9HAZK4/0.jpg', 'Automate repetitive tasks and simplify your workflow using Python', 'coding');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (3, 'Space title 2', 'https://www.youtube.com/watch?v=lqc0AQ3Rx7o', 'https://img.youtube.com/vi/lqc0AQ3Rx7o/0.jpg', 'Mysteries of the Universe', 'space');
+VALUES (1, 'Building RESTful APIs with Node.js and Express', 'https://www.youtube.com/watch?v=TC1oVXlVE3M', 'https://img.youtube.com/vi/TC1oVXlVE3M/0.jpg', 'Create robust and scalable RESTful APIs using Node.js and Express', 'coding');
 
 INSERT INTO resources(user_id, title, url, img_url, description, category)
-VALUES (3, 'Space title 3', 'https://www.youtube.com/watch?v=hyNwBiCs7Ro', 'https://img.youtube.com/vi/hyNwBiCs7Ro/0.jpg', 'Unexplained Universe Mysteries', 'space');
+VALUES (3, 'Data Structures and Algorithms', 'https://www.youtube.com/watch?v=GWYhtksrmhE', 'https://img.youtube.com/vi/GWYhtksrmhE/0.jpg', 'Master fundamental data structures and algorithms to enhance your coding skills', 'coding');
 
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (1, 'Exploring the Wonders of Space', 'https://www.youtube.com/watch?v=kQNqmUZrEOk', 'https://img.youtube.com/vi/kQNqmUZrEOk/0.jpg', 'Embark on a captivating journey through the vast wonders of the cosmos', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (1, 'Secrets of Black Holes: Gateways to the Unknown', 'https://www.youtube.com/watch?v=UVPALTZhX_M', 'https://img.youtube.com/vi/UVPALTZhX_M/0.jpg', 'Unravel the mind-bending secrets and extraordinary gateways that black holes hold within the vast fabric of the cosmos', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (1, 'Python for Data Science: Analyzing Data', 'https://www.youtube.com/watch?v=N7ZmPYaXoic', 'https://img.youtube.com/vi/N7ZmPYaXoic/0.jpg', 'Utilize Python for data analysis and visualization to extract insights from datasets', 'coding');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (1, 'Java Programming: Beginner to Advanced', 'https://www.youtube.com/watch?v=hUdB232fIXM', 'https://img.youtube.com/vi/hUdB232fIXM/0.jpg', 'Embark on a comprehensive journey through Java programming, from beginner to advanced concepts', 'coding');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Unveiling the Mysteries of the Universe', 'https://www.youtube.com/watch?v=lqc0AQ3Rx7o', 'https://img.youtube.com/vi/lqc0AQ3Rx7o/0.jpg', 'Discover mind-boggling phenomena and unravel the enigmatic mysteries of the vast universe', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Search for Extraterrestrial Life', 'https://www.youtube.com/watch?v=bZEEVwa5H78', 'https://img.youtube.com/vi/bZEEVwa5H78/0.jpg', 'Embark on an extraordinary quest to explore the possibility of life beyond Earth and the search for extraterrestrial civilizations', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Cosmic Time Travel: Exploring the Early Universe', 'https://www.youtube.com/watch?v=F-j4fZT1JgM', 'https://img.youtube.com/vi/F-j4fZT1JgM/0.jpg', 'Journey back in time and witness the mind-boggling origins of our cosmos as scientists explore the early universe', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (1, 'Journey to the Edge of the Universe', 'https://www.youtube.com/watch?v=mO3Q4bRQZ3k', 'https://img.youtube.com/vi/mO3Q4bRQZ3k/0.jpg', 'Embark on an extraordinary and awe-inspiring voyage to the very edge of the vast cosmos', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Scale of the Universe: From Small to Large', 'https://www.youtube.com/watch?v=I8N0KWH3-58', 'https://img.youtube.com/vi/I8N0KWH3-58/0.jpg', 'Explore the mind-boggling scales of the universe, from the tiniest particles to the vast cosmic structures', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (2, 'Mastering Coding Concepts', 'https://www.youtube.com/watch?v=B_n4YONte5A', 'https://img.youtube.com/vi/B_n4YONte5A/0.jpg', 'Unlock the secrets of coding and master the fundamental concepts to become a skilled developer', 'coding');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (2, 'Building Web Apps with React', 'https://www.youtube.com/watch?v=NzpbupWoIV4', 'https://img.youtube.com/vi/NzpbupWoIV4/0.jpg', 'Learn the fundamentals of React and leverage its power to build dynamic web applications', 'coding');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Unraveling Enigmatic Universe Phenomena', 'https://www.youtube.com/watch?v=hyNwBiCs7Ro', 'https://img.youtube.com/vi/hyNwBiCs7Ro/0.jpg', 'Delve into the mysteries that perplex astronomers and scientists as they unravel enigmatic phenomena of the vast universe', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Intro to HTML and CSS: Building Web Pages', 'https://www.youtube.com/watch?v=jxsgV2j0Xug', 'https://img.youtube.com/vi/jxsgV2j0Xug/0.jpg', 'Get started with HTML and CSS and create attractive and interactive web pages', 'coding');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Database Design and SQL: Comprehensive Guide', 'https://www.youtube.com/watch?v=G0bBLvWXBvc', 'https://img.youtube.com/vi/G0bBLvWXBvc/0.jpg', 'Master database design principles and SQL queries with this comprehensive guide', 'coding');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (1, 'Secrets of Black Holes: Gateways to the Unknown', 'https://www.youtube.com/watch?v=UVPALTZhX_M', 'https://img.youtube.com/vi/UVPALTZhX_M/0.jpg', 'Unravel the mind-bending secrets and extraordinary gateways that black holes hold within the vast fabric of the cosmos', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Astrobiology: Science of Life in the Universe', 'https://www.youtube.com/watch?v=_IkaetPoBZM', 'https://img.youtube.com/vi/_IkaetPoBZM/0.jpg', 'Discover the captivating and evolving field of astrobiology, where scientists explore the possibilities of life in the vast universe', 'space');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (2, 'C++ Crash Course: Basics in One Hour', 'https://www.youtube.com/watch?v=1v_4dL8l8pQ', 'https://img.youtube.com/vi/1v_4dL8l8pQ/0.jpg', 'Get up to speed with C++ programming in just one hour with this crash course', 'coding');
+
+INSERT INTO resources(user_id, title, url, img_url, description, category)
+VALUES (3, 'Building Web Apps with Ruby', 'https://www.youtube.com/watch?v=Z0Xn1iiiEZE', 'https://img.youtube.com/vi/Z0Xn1iiiEZE/0.jpg', 'Learn to build web applications using Ruby on Rails framework through this comprehensive course', 'coding');
